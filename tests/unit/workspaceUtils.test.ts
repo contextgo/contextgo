614dde55374c75e7bb8fdbe63b781d85d16b5c1a
yeyitech <yeyitech@users.noreply.github.com>
2026-04-17 13:25:44 +0800

    test(workspace): include runtime policy path in utils expectation

--- tests/unit/workspaceUtils.test.ts
+++ tests/unit/workspaceUtils.test.ts
@@ -33,6 +33,7 @@ describe('workspace utils', () => {
       hooksFile: '/Users/demo/project/.contextgo/hooks.json',
       commandsFile: '/Users/demo/project/.contextgo/commands.json',
       schedulesFile: '/Users/demo/project/.contextgo/schedules.json',
+      runtimePolicyFile: '/Users/demo/project/.contextgo/runtime.json',
     });
   });
 
@@ -44,6 +45,7 @@ describe('workspace utils', () => {
       hooksFile: 'D:\\workspace\\project\\.contextgo\\hooks.json',
       commandsFile: 'D:\\workspace\\project\\.contextgo\\commands.json',
       schedulesFile: 'D:\\workspace\\project\\.contextgo\\schedules.json',
+      runtimePolicyFile: 'D:\\workspace\\project\\.contextgo\\runtime.json',
     });
   });
 
